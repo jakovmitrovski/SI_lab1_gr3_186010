1fc76f01730ecba4fb3598d39b5665ff78a686d3
Jakov <jakovvmitrovski@gmail.com>
2020-03-22 13:59:21 +0100

    taxReturn method finished

--- Item.java
+++ Item.java
@@ -10,7 +10,51 @@ class Item {
 
 	//TODO setters and getters
 
+
+	public Item(int id, String name, double price, char taxType) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.taxType = taxType;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public void setTaxType(char taxType) {
+		this.taxType = taxType;
+	}
+
+	public char getTaxType() {
+		return taxType;
+	}
+
 	double taxReturn () {
-		//TODO
+		double danok = 0;
+		if (taxType == 'А') danok = price*0.18;
+		else if (taxType == 'Б') danok = price*0.05;
+
+		return danok*0.15;
 	}
 }
